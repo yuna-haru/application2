1a8cb438f6bd95d53252babfc3adf86c649be27e
yuna-haru <skywar.love22@gmail.com>
2022-04-02 14:58:06 +0000

    application2 16syou finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+    #特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し@post_imagesに渡すという処理
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
