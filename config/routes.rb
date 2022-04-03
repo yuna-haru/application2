1a252f66bed97488db7cab56740b8726457e0352
yuna-haru <skywar.love22@gmail.com>
2022-04-03 06:09:14 +0000

    application2 18syou finish

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    #post_commentsは、post_imagesに結びつく親子関係のようになる
+  end
+
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
