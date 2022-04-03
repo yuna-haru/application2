18b363210fb917ddd8724a01bf298da592b4c74a
yuna-haru <skywar.love22@gmail.com>
2022-04-03 12:54:53 +0000

    24syou start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
     #特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し@post_imagesに渡すという処理
   end
 
